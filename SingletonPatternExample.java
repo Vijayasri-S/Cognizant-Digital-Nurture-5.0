c086602961ae74e741e7f7bd6682ab036a643262
Vijayasri-S <svijayasri2004@gmail.com>
2026-06-24 20:14:33 +0530

    Document Singleton Pattern in Logger class
    Added comments explaining the Singleton Pattern and its implementation in the Logger class.

--- SingletonPatternExample.java
+++ SingletonPatternExample.java
@@ -1,3 +1,11 @@
+/* Singleton Pattern is creational pattern where a class can only have one instance
+    Example: In a school there can be multiple instance to student class but only one instance to Principal class as there is only one principal in a school.
+    To make this possible we make the constructor of the class private so object can't be created outside the class.
+
+    In this activity Logger has only one instance "instance"
+    To make it thread safe object is created imediately as the class is loaded. And Final keyword makes sure that there is only one instance.
+    l1==l2 gives true as output which means both point to same instance*/
+    
 class Logger{
     private static final Logger instance=new Logger();
     private Logger(){
@@ -10,8 +18,7 @@ class Logger{
 public class SingletonPatternExample{
     public static void main(String[] args){
         Logger l1=Logger.getInstance ();
-    Logger l2=Logger.getInstance();
-        //SingletonPatternExample s=new SingletonPatternExample();
+        Logger l2=Logger.getInstance();
         System.out.println(l1);
         System.out.println(l2);
         System.out.println(l1==l2);
